46b5677b81eb8feb74f2a701caa72c17449c3c20
Luiz Felipe Soares <38364154+luizfsoares@users.noreply.github.com>
2019-09-10 20:30:10 -0300

    Update MemoriaVirtual.java

--- Projeto2 SO/MemoriaVirtual.java
+++ Projeto2 SO/MemoriaVirtual.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package memoriavirtual;
 
 import java.io.IOException;
 import java.nio.file.Files;
